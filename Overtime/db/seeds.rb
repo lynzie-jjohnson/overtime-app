86e9c7e5bef70695e6549deedca72b3cca85376c
Lynzie Johnson <lynzie.jjohnson@gmail.com>
2017-11-14 14:12:07 -0700

    updated seed file

--- Overtime/db/seeds.rb
+++ Overtime/db/seeds.rb
@@ -1,5 +1,8 @@
+@user = User.create(email: "test@test.com", password: "123456", password_confirmation: "123456", first_name: "John", last_name: "Son")      
+
+puts "1 User Created"
 100.times do |post|
-    Post.create!(date: Date.today, rationale: "#{post} rationale content")
+    Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "100 posts have been created"
