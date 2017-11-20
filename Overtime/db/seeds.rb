91d28fc3227f4ec5040408bd38cfc401ab7bbb9f
Lynzie Johnson <lynzie.jjohnson@gmail.com>
2017-11-20 12:04:58 -0700

    Implemented edit funtionality for the status

--- Overtime/db/seeds.rb
+++ Overtime/db/seeds.rb
@@ -1,6 +1,11 @@
 @user = User.create(email: "test@test.com", password: "123456", password_confirmation: "123456", first_name: "John", last_name: "Son")      
 
 puts "1 User Created"
+
+AdminUser.create(email: "admin@test.com", password: "123456", password_confirmation: "123456", first_name: "Admin", last_name: "Name")      
+
+puts "1 Admin User Created"
+
 100.times do |post|
     Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
