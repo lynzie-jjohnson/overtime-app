95e07c16d7d0f8f78e218c83af3e64c4eded8c27
Lynzie Johnson <lynzie.jjohnson@gmail.com>
2017-11-30 14:12:52 -0700

    Built in approval and review buttons for admin review homepage

--- Overtime/app/controllers/posts_controller.rb
+++ Overtime/app/controllers/posts_controller.rb
@@ -1,52 +1,60 @@
 class PostsController < ApplicationController
-    before_action :set_post, only: [:show, :edit, :update, :destroy]
-    def index 
-        @posts = Post.posts_by(current_user).page(params[:page]).per(10)
-    end 
+	before_action :set_post, only: [:show, :edit, :update, :destroy, :approve]
+	
+	def index
+		@posts = Post.posts_by(current_user).page(params[:page]).per(10)
+	end
 
-    def new
-        @post = Post.new
-    end
+	def approve
+		authorize @post
+		@post.approved!
+		redirect_to root_path, notice: "The post has been approved"
+	end
 
-    def create
-        @post = Post.new(post_params)
-        @post.user_id = current_user.id
+	def new
+		@post = Post.new
+	end
 
-        if @post.save
-            redirect_to @post, notice: "Your post was created successfully"
-        else
-            render :new
-        end
-    end
+	def create
+		@post = Post.new(post_params)
+		@post.user_id = current_user.id
 
-    def update
-        authorize @post
-        if @post.update(post_params)
-            redirect_to @post, notice: "Your post was edited successfully"
-        else
-            render :edit
-        end
-    end
+		if @post.save
+			redirect_to @post, notice: 'Your post was created successfully'
+		else
+			render :new
+		end
+	end
 
-    def edit
-        authorize @post
-    end
+	def edit
+		authorize @post
+	end
 
-    def show  
-    end
+	def update
+		authorize @post
+		
+		if @post.update(post_params)
+			redirect_to @post, notice: 'Your post was edited successfully'
+		else
+			render :edit
+		end
+	end
 
-    def destroy
-        @post.delete
-        redirect_to posts_path, notice: "Your post was destroyed successfully"
-    end
+	def show
+	end
 
-    private
+	def destroy
+    @post.delete
+    redirect_to posts_path, notice: 'Your post was deleted successfully'
+	end
 
-    def post_params
-        params.require(:post).permit(:date, :rationale, :status, :overtime_request)
-    end
+	private
 
-    def set_post
-        @post = Post.find(params[:id])
-    end
+	  def post_params
+	  	params.require(:post).permit(:date, :rationale, :status, :overtime_request)
+	  end
+
+	  def set_post
+	  	@post = Post.find(params[:id])
+	  end
 end
